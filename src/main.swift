c013a591f318a14dd8d310d5fc27b5b07c9e174d
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-06 00:13:24 +0200

    [src/main.swift] command line args

--- src/main.swift
+++ src/main.swift
@@ -1,1 +1,12 @@
-print("Hello, World!")
+// Helper function that crashes the application
+func crash(_ message: String) -> Never {
+    print("todo: Fatal error: \(message)")
+    exit(1)
+}
+
+// Command line arguments
+var args = CommandLine.arguments
+
+// Validating args 
+args.removeFirst()
+if args.count == 0 { crash("Not enough arguments provided.") }
