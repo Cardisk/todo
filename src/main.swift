17572795fe6ba3e5e18e4c5a57680068b9a56ead
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-06 00:57:35 +0200

    [src/main.swift] adding error codes

--- src/main.swift
+++ src/main.swift
@@ -1,7 +1,11 @@
+enum Errno: Int32 {
+    case few_args = 1
+}
+
 // Helper function that crashes the application
-func crash(_ message: String) -> Never {
-    print("todo: Fatal error: \(message)")
-    exit(1)
+func crash(_ message: String, _ code: Errno) -> Never {
+    print("Fatal error: \(message)")
+    exit(code.rawValue)
 }
 
 // Command line arguments
@@ -9,4 +13,4 @@ var args = CommandLine.arguments
 
 // Validating args 
 args.removeFirst()
-if args.count == 0 { crash("Not enough arguments provided.") }
+if args.count == 0 { crash("Not enough arguments provided.", .few_args) }
