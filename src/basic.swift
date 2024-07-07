1f0502f5855d28fbcbce3c4f77116f2b1e496f50
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-08 01:29:18 +0200

    [src/basic.swift] variants for 'crash' func

--- src/basic.swift
+++ src/basic.swift
@@ -1,11 +1,40 @@
 // Error number 
 enum Errno: Int32 {
-    case few_args = 1
+    case generic = 1
+    case few_args, command, todo
+    case count
 }
 
 // Helper function that crashes the application
 func crash(_ message: String, _ code: Errno) -> Never {
-    print("Fatal error: \(message)")
+    print("fatal: \(message): \(errnoMsg(code))")
     exit(code.rawValue)
 }
 
+// Helper function that crashes the application
+func crash(_ message: String) -> Never {
+    print("fatal: \(message)")
+    exit(Errno.generic.rawValue)
+}
+
+// Helper function that crashes the application
+func crash(_ code: Errno) -> Never {
+    print("fatal: \(errnoMsg(code))")
+    exit(code.rawValue)
+}
+
+private func errnoMsg(_ code: Errno) -> String {
+    assert(Errno.count.rawValue == 5, "ERROR: Errno enum messages not fully handled.")
+    switch code {
+        case .generic:
+            return "A generic error occured."
+        case .few_args:
+            return "Not enough arguments provided."
+        case .command:
+            return "Unknown command provided."
+        case .todo:
+            return "Not implemented yet."
+        default:
+            return ""
+    }
+}
