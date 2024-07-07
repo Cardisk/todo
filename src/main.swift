bf42d4fb99586367e46af174d9341ea122002f70
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-07 16:57:24 +0200

    [src/main.swift] cleaning a bit

--- src/main.swift
+++ src/main.swift
@@ -1,16 +1,7 @@
-enum Errno: Int32 {
-    case few_args = 1
-}
-
-// Helper function that crashes the application
-func crash(_ message: String, _ code: Errno) -> Never {
-    print("Fatal error: \(message)")
-    exit(code.rawValue)
-}
-
 // Command line arguments
 var args = CommandLine.arguments
 
 // Validating args 
 args.removeFirst()
 if args.count == 0 { crash("Not enough arguments provided.", .few_args) }
+else { print("Called Succesfully!") }
