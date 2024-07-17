154f6ecec4c1f4f332339cfff1a9c0d9e024d4d6
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-17 23:28:49 +0200

    [src/issue.swift] adding data containing title and body

--- src/issue.swift
+++ src/issue.swift
@@ -1,14 +1,28 @@
+import Foundation
+
 struct Issue: Codable {
     var filePath: String
     var title: String
     var rawTitle: String
     var body: String
+    var data: Data
 
     init(_ filePath: String, _ title: String, _ rawTitle: String, _ body: String) {
         self.filePath = filePath
         self.title = title
         self.rawTitle = rawTitle
         self.body = body
+
+        let dict = [
+            "title": self.title,
+            "body": self.body,
+        ]
+
+        do {
+            self.data = try JSONSerialization.data(withJSONObject: dict)
+        } catch {
+            self.data = Data()
+        }
     }
 }
 
