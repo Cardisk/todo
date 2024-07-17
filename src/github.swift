613b3baa559d7ac0975d47a3aac0003cee387beb
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-17 23:30:57 +0200

    [src/github.swift] implementing API interactions

--- src/github.swift
+++ src/github.swift
@@ -1,9 +1,16 @@
 import Foundation
 
+struct GithubIssue: Codable {
+    let number: Int
+    let title: String
+    let body: String
+}
+
 struct Github {
     private var token: String
     private var owner: String
     private var repo: String
+    private var request: URLRequest
 
     init(_ settings: Settings) {
         self.token = getEnv("GITHUB_TOKEN") 
@@ -36,9 +43,61 @@ struct Github {
             self.repo = info[1]
 
         }
+        
+        let urlString = "https://api.github.com/repos/\(self.owner)/\(self.repo)/issues"
+        guard let url = URL(string: urlString) else {
+            crash(urlString, .invalidURL)
+        }
+
+        self.request = URLRequest(url: url)
+
+        self.request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        self.request.addValue("application/vnd.github+json", forHTTPHeaderField: "Accept")
+        self.request.addValue("token \(self.token)", forHTTPHeaderField: "Authorization")
     }
 
-    func getIssues() -> Void {}
+    mutating func getIssues() async -> [GithubIssue] {
+        var githubIssues: [GithubIssue] = []
 
-    func postIssues(_ issues: [Issue]) -> Void {}
+        self.request.httpMethod = "GET"
+        self.request.httpBody = nil
+
+        do {
+            let (data, response) = try await query(self.request)
+
+            guard let code = response as? HTTPURLResponse, 200 ~= code.statusCode else {
+                crash("Github.getIssues: \(self.request.url!.path())", .invalidResponse)
+            }
+
+            githubIssues = try JSONDecoder().decode([GithubIssue].self, from: data)
+        } catch {
+            crash(error.localizedDescription)
+        } 
+
+        return githubIssues
+    }
+
+    mutating func postIssues(_ issues: [Issue]) async -> [GithubIssue] {
+        var githubIssues: [GithubIssue] = []
+
+        self.request.httpMethod = "POST"
+
+        do {
+            for issue in issues {
+                self.request.httpBody = issue.data
+                let (data, response) = try await query(self.request)
+                
+                guard let code = response as? HTTPURLResponse, 201 ~= code.statusCode else {
+                    crash("Github.postIssues: response \(response) at \(self.request.url!.path())", .invalidResponse)
+                }
+
+                let githubIssue = try JSONDecoder().decode(GithubIssue.self, from: data)
+                githubIssues.append(githubIssue)
+            }
+        } catch {
+            crash(error.localizedDescription)
+        }
+
+        return githubIssues
+    }
 }
