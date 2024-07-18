87f101f9c66655f473811ac5be46d23fdeeb2623
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-18 22:57:56 +0200

    [src/issue.swift] adding issue number

--- src/issue.swift
+++ src/issue.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct Issue: Codable {
     var filePath: String
+    var number: Int
     var title: String
     var rawTitle: String
     var body: String
@@ -9,6 +10,7 @@ struct Issue: Codable {
 
     init(_ filePath: String, _ title: String, _ rawTitle: String, _ body: String) {
         self.filePath = filePath
+        self.number = 0
         self.title = title
         self.rawTitle = rawTitle
         self.body = body
