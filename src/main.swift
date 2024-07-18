fe82b41506d79948ca05502696aeaf84522d3bcf
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-18 22:59:03 +0200

    [src/main.swift] reimplementing main.swift I didn't like the old version so I decided to rework the things a little bit...

--- src/main.swift
+++ src/main.swift
@@ -1,130 +1,200 @@
 import Foundation
 
-// Global variables
+// JSON serializers
 let jsonEncoder = JSONEncoder()
 let jsonDecoder = JSONDecoder()
-let settings: Settings = if File.exists("todo.json") {
-    try! jsonDecoder.decode(Settings.self, from: File("todo.json").contentData)
-} else {
-    Settings()
+
+// application settings
+var settings = Settings()
+
+if FileManager().fileExists(atPath: "todo.json") {
+     do {
+         settings = try jsonDecoder.decode(Settings.self, 
+                                 from: File("todo.json").contentData)
+     } catch {
+         crash(error.localizedDescription)
+     }
 }
-var github = Github(settings)
 
-// Command line arguments
-var args = CommandLine.arguments
-var issueCollection: [(File, [Issue])] = []
-
-let commands: [String: () -> Void] = [
+// available commands list
+let commands: [String: ([String]) -> Void] = [
     "-h": help,
     "help": help,
+
     "-s": store,
     "store": store,
-    "-c": commit, 
-    "commit": commit, 
+
+    "-c": commit,
+    "commit": commit,
+
+    "-g": get,
+    "get": get,
 ]
 
-func help() -> Void {
+// commands functions
+func help(_ args: [String]) -> Void {
     print("""
-    USAGE: 
-        
-        todo [ -s | store ] [ -c | commit ] [ -h | help ] <file> ...
+    NAME:
+            todo - utility to handle TODOs and FIXMEs
+
+    USAGE:
+            todo [ -h | help   ] [ -s | store  ] 
+                 [ -c | commit ] [ -g | get    ] <file> ...
+
+    DESCRIPTION:
+            Todo is a command line utility to process TODOs and FIXMEs inside
+            the file[s] provided as argument[s].
+            If no option is provided, the program will be executed in its
+            entirety.
+            But what exactly does this program do?
+            It will scan the file[s] searching TODOs and FIXMEs, then it asks 
+            the user to add or discard them interactively. 
+            Based on which command is being executed, the program handles
+            issues differently (see OPTIONS below).
+            At the end of the process, only accepted issues will be posted on
+            GitHub and TODOs and FIXMEs are replaced with ISSUE.
 
     OPTIONS:
+            [ -h | help   ]
+                Show this message
 
-        [ -s | store ] <file> ...
-            store the issues on a temporary file
-        
-        [ -c | commit ]
-            commit the issues saved on the temporary file
-        
-        [ -h | help ]
-            show this help message
+            [ -s | store  ]
+                Skip the GitHub posting and store the result into '.todoIssues'
 
+            [ -c | commit ]
+                Read the issues from '.todoIssues' and post them onto GitHub
+                Also changes the interested file[s]
+
+            [ -g | get    ]
+                Display all the issues found on GitHub
     """)
 }
 
-func store() -> Void {
+func store(_ args: [String]) -> Void {
     if args.isEmpty { crash(.fewArgs) }
-    todo(commitToFile: false)
+    // TODO: implement here
+    crash(.todo)
 }
 
-func commit() -> Void {
-    do {
-        let data = File(".todoIssues").contentData
-        let issues: [Issue] = try jsonDecoder.decode([Issue].self, from: data)
-        for issue in issues { print(issue) }
-    } catch { crash(error.localizedDescription) }
+func commit(_ args: [String]) -> Void {
+    // TODO: implement here
+    crash(.todo)
 }
 
-func todo(commitToFile: Bool = true) {
-    var files: [File] = []   
+func get(_ args: [String]) -> Void {
+    // TODO: implement here
+    crash(.todo)
+}
+
+// application functions
+func files(_ args: [String]) -> [File] {
+    var files: [File] = []
+
     for arg in args {
-        let f = File(arg, settings.prefix, settings.postfix)
-        files.append(f)
+        let file = File(arg, settings.prefix, settings.postfix)
+        files.append(file)
     }
 
-    for f in files {
-        var issues = f.issues()
-        var i = 0
+    return files
+}
+
+func issues(_ files: [File]) -> [Issue] {
+    var issues: [Issue] = []
+    
+    for file in files {
+        let fileIssues = file.issues()
+        if !fileIssues.isEmpty { issues.append(contentsOf: fileIssues) }
+    }
+
+    return issues
+}
+
+func user(_ issues: inout [Issue]) -> Void {
+    if issues.isEmpty { return }
+
+    var i = 0
+    let prompt = "    [ \(GREEN)a\(RESET)dd / \(RED)D\(RESET)ISCARD ] > "
+    
+    print()
+    
+    while i < issues.count {
+        print(issues[i])
+        print(prompt, terminator: "")
+
+        let choice = readLine(strippingNewline: true) ?? ""
+        switch choice.first {
+        case "a":
+            i += 1
+        default:
+            issues.remove(at: i)
+        }
         print()
-        while i < issues.count {
-            print(issues[i])
-            let prompt = "    [ \(GREEN)a\(RESET)dd / \(RED)D\(RESET)ISCARD ] > "
-            print(prompt, terminator: "")
-            let choice = readLine(strippingNewline: true) ?? ""
-            switch choice.first {
-            case "a":
-                i += 1
-            default:
-                issues.remove(at: i)
-            }
-            print()
-        }
-
-        if issues.isEmpty { continue }
-        issueCollection.append((f, issues))
-    }
-
-    var jsonData: Data = Data()
-    for ic in issueCollection {
-        if commitToFile {
-            // <file>.commitIssues(<issues>)
-            ic.0.commitIssues(ic.1)
-        } else {
-            do {
-                let data = try jsonEncoder.encode(ic.1)
-                jsonData += data
-            } catch {
-                crash(error.localizedDescription)
-            }
-        }
-    }
-
-    if !jsonData.isEmpty {
-        do {
-            try jsonData.write(to: URL(fileURLWithPath: ".todoIssues"))
-        } catch {
-            crash(error.localizedDescription)
-        }
     }
 }
 
+func post(_ issues: [Issue]) async -> [GithubIssue] {
+    var handle = await Github(settings)
+    let githubIssues = await handle.postIssues(issues)
+    return githubIssues
+}
 
-func main() -> Never {
-    // Validating args 
+func modify(_ files: [File], _ issues: [Issue]) -> Void {
+    for file in files {
+        var fileIssues: [Issue] = []
+        for issue in issues {
+            if file.path != issue.filePath { continue }
+            fileIssues.append(issue)
+        }
+        if !fileIssues.isEmpty { file.commitIssues(fileIssues) }
+    }
+}
+
+func main() async -> Never {
+    // command line arguments
+    var args = CommandLine.arguments
+    // removing program name
     args.removeFirst()
+    // nothing provided
     if args.isEmpty { crash(.fewArgs) }
+    
 
+    // safe unwrap, already validated
     switch args.first! {
     case let cmd where commands.keys.contains(cmd):
+        // removing the command
         args.removeFirst()
-        commands[cmd]!()
+        // safe unwrap, already validated
+        commands[cmd]!(args)
 
     default:
-        todo()
+        let files = files(args)
+        var issues = issues(files)
+        user(&issues)
+
+        if issues.isEmpty {
+            print("Nothing to do here...")
+            exit(0)
+        }
+
+        let githubIssues = await post(issues)
+
+        if issues.count != githubIssues.count { crash(.invalidIssuesCount) }
+        var i = 0
+        while i < issues.count {
+            issues[i].number = githubIssues[i].number
+            i += 1
+        }
+
+        modify(files, issues)
+
+        if !issues.isEmpty {
+            print("Issue", terminator: "")
+            print(issues.count > 1 ? "s " : " ", terminator: "")
+            print("submitted!")
+        }
     }
 
     exit(0)
 }
 
-main()
+await main()
